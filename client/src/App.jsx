d8abdf21cf5a1e62b8ae34613e894fdd6e06a159
Andrey Vershinin <adw006@yandex.com>
2018-09-07 20:50:44 +0300

    add dashboard profile state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import jwtDecode from 'jwt-decode';
 
 import store from './store';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser, logoutUser } from './actions/authAction';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 
 import Navbar from './components/layout/Navbar.jsx';
 import Footer from './components/layout/Footer.jsx';
 import Landing from './components/layout/Landing.jsx';
 import Register from './components/auth/Register.jsx';
 import Login from './components/auth/Login.jsx';
+import Dashboard from './components/dashboard/Dashboard.jsx';
+import PrivateRoute from './components/common/PrivateRoute.jsx';
+import CreateProfile from './components/create-profile/CreateProfile.jsx';
 
 import './App.css';
 
@@ -28,7 +32,7 @@ if (localStorage.jwtToken) {
   if (decodedData.exp < currentTime) {
     store.dispatch(logoutUser());
     // TODO: Clear current profile
-
+    store.dispatch(clearCurrentProfile());
     // Redirect to login
     window.location.href = '/login';
   }
@@ -46,6 +50,12 @@ class App extends Component {
             <div className="container">
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <Switch>
+                <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path='/create-profile' component={CreateProfile} />
+              </Switch>
             </div>
             <Footer />
           </div>
